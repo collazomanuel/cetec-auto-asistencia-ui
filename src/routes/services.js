39fb02c37085d92dadb5519aae10bbdd6c57278d
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-03-17 23:12:11 -0300

    Parameterize back-end URL

--- src/routes/services.js
+++ src/routes/services.js
@@ -2,7 +2,7 @@ import { sendPostRequest } from './requestQueue';
 
 const registerStudent = async (email, image, latitude, longitude, accuracy) => {
 	try {
-		const url = 'http://localhost:8000' + '/student';
+		const url = process.env.BACKEND_URL + '/student';
 		const body = { email: email, image: image, latitude: latitude, longitude: longitude, accuracy: accuracy };
 		const response = sendPostRequest(url, body);
 		return response.data;
