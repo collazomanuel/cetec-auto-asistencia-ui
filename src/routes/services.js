ae11c07de83a657371e1a4fb90b346f9e1c170f1
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-03-18 00:00:31 -0300

    Fix environmental variable

--- src/routes/services.js
+++ src/routes/services.js
@@ -2,7 +2,7 @@ import { sendPostRequest } from './requestQueue';
 
 const registerStudent = async (email, image, latitude, longitude, accuracy) => {
 	try {
-		const url = process.env.BACKEND_URL + '/student';
+		const url = import.meta.env.VITE_API_URL + '/student';
 		const body = { email: email, image: image, latitude: latitude, longitude: longitude, accuracy: accuracy };
 		const response = sendPostRequest(url, body);
 		return response.data;
