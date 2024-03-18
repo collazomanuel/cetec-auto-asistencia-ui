5c586cf08f031a9c6c52a07c2664ba2b30eca6e5
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-03-17 22:47:40 -0300

    Remove back-end

--- backend/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-from decouple import config
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MONGODB_KEY = config('MONGODB_KEY')
-BACKEND_URL = config('BACKEND_URL')
