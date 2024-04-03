120ea2866c82bb0df52bd6fe89a07b1295d81410
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-04-03 20:33:02 -0300

    Use env variable instead of hardcoded string

--- src/routes/services.js
+++ src/routes/services.js
@@ -2,7 +2,7 @@ import { sendPostRequest } from './requestQueue';
 
 const registerStudent = async (email, image, latitude, longitude, accuracy) => {
 	try {
-		const url = 'https://cetec-auto-asistencia-backend.onrender.com' + '/student';
+		const url = import.meta.env.VITE_API_URL + '/student';
 		const body = { email: email, image: image, latitude: latitude, longitude: longitude, accuracy: accuracy };
 		const response = sendPostRequest(url, body);
 		return response.data;
