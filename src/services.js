265e6537a028e8f7b141a93ddde08ec6579fa0e6
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-06-26 19:37:57 -0300

    Change request method

--- src/services.js
+++ src/services.js
@@ -1,10 +1,10 @@
-import { sendPostRequest } from './requestQueue';
+import { sendPutRequest } from './requestQueue';
 
 const registerStudent = async (email, image, latitude, longitude, accuracy) => {
 	try {
 		const url = import.meta.env.VITE_API_URL + '/student';
 		const body = { email: email, image: image, latitude: latitude, longitude: longitude, accuracy: accuracy };
-		const response = sendPostRequest(url, body);
+		const response = sendPutRequest(url, body);
 		return response.data;
 	} catch (error) {
 		console.error(error);
