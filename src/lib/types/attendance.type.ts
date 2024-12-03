6f12a75cc65b1105d54022fed48d322d193ada2c
Manuel Collazo <collazo.manuel.28@gmail.com>
2024-12-03 18:44:55 -0300

    Refactor type definitions

---
+++ src/lib/types/attendance.type.ts
@@ -0,0 +1,9 @@
+
+export type AttendanceType = {
+    email: string;
+    code: string;
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+    image: string;
+};
--- src/lib/types/exam.type.ts
+++ src/lib/types/exam.type.ts
@@ -1,22 +1,8 @@
 
-export type UserType = {
-    email: string;
-    image: string;
-};
-
 export type ExamType = {
     code: string;
     name: string;
     start: string;
     length: number;
     margin: number;
-};
-
-export type AttendanceType = {
-    email: string;
-    code: string;
-    latitude: number;
-    longitude: number;
-    accuracy: number;
-    image: string;
 };
