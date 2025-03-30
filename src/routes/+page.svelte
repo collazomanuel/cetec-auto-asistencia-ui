ef9236d343a4532e16d527a8079f0ab283dd265c
Manuel Collazo <collazo.manuel.28@gmail.com>
2025-03-30 17:38:32 -0300

    Add button to sign out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../styles.css';
 	import Detector from '../Detector.svelte';
-	import { signIn } from '@auth/sveltekit/client';
+	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
 </script>
 
@@ -11,6 +11,9 @@
 			user={$page.data.session?.user}
 			userToken={$page.data.session?.access_token}
 		/>
+		<button class="button" on:click={() => signOut()}>
+			Cerrar sesión
+		</button>
 	{:else}
 		<button class="button" on:click={() => signIn('google')}>
 			Iniciar sesión con Google
--- src/routes/exam/+page.svelte
+++ src/routes/exam/+page.svelte
@@ -1,13 +1,16 @@
 <script>
 	import '../../styles.css';
 	import ExamController from '../../ExamController.svelte';
-	import { signIn } from '@auth/sveltekit/client';
+	import { signIn, signOut } from '@auth/sveltekit/client';
 	import { page } from '$app/stores';
 </script>
 
 <div class="app">
 	{#if $page.data.session}
 		<ExamController userToken={$page.data.session.access_token ?? ''} />
+		<button class="button logout" on:click={() => signOut()}>
+			Cerrar sesión
+		</button>
 	{:else}
 		<button class="button" on:click={() => signIn('google')}>
 			Iniciar sesión con Google
