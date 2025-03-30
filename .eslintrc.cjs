e9ecc46da418fdabeac4121e6afcae1f5e610ce0
Manuel Collazo <collazo.manuel.28@gmail.com>
2025-03-30 13:54:33 -0300

    Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,34 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+    root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:svelte/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+        extraFileExtensions: ['.svelte'],
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json']
+    },
+    plugins: ['@typescript-eslint'],
+    env: {
+        browser: true,
+        es2017: true,
+        node: true
+    },
+    rules: {
+        // Example rules (customize as needed)
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        'svelte/no-at-html-tags': 'warn',
+		'svelte/no-at-html-tags': 'warn', // Warn when using @html to avoid XSS risks
+		'svelte/valid-compile': 'error', // Ensure valid Svelte syntax
+		'svelte/no-unused-class-name': 'warn' // Warn about unused CSS class names
+    }
 };
